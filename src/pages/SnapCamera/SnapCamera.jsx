ff58e901d7df5bf01e03905db967d6a664a2f6ab
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2025-04-05 15:34:17 +0530

    dd

--- src/pages/SnapCamera/SnapCamera.jsx
+++ src/pages/SnapCamera/SnapCamera.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import { bootstrapCameraKit, CameraKitSession, createMediaStreamSource, Transform2D, Lens } from "@snap/camera-kit";
+import React, { useEffect, useRef, useCallback } from 'react';
+import { bootstrapCameraKit, createMediaStreamSource, Transform2D } from "@snap/camera-kit";
 import './SnapCamera.css';
+
 let mediaStream;
 
 const SnapCamera = () => {
@@ -10,47 +11,22 @@ const SnapCamera = () => {
   const cameraSelectRef = useRef(null);
   const lensSelectRef = useRef(null);
 
-  useEffect(() => {
-    const init = async () => {
-      const cameraKit = await bootstrapCameraKit({ apiToken: apiToken });
-
-      const session = await cameraKit.createSession();
-
-      // Use the ref to get the canvas element
-      const canvas = canvasRef.current;
-      if (canvas) {
-        canvas.replaceWith(session.output.live);
-      }
-      const { lenses } = await cameraKit.lenses.repository.loadLensGroups([lensGroupId]);
-      session.applyLens(lenses[19]);
-      await setCameraKitSource(session);
-      await attachCamerasToSelect(session);
-      console.log('attachCamerasToSelect is called');
-      await attachLensesToSelect(lenses, session);
-      console.log('attachLensesToSelect is called');
-    };
-
-    init();
-  }, [apiToken, lensGroupId]);
-
-  const setCameraKitSource = async (session, deviceId) => {
+  const setCameraKitSource = useCallback(async (session, deviceId) => {
     if (mediaStream) {
       session.pause();
       mediaStream.getVideoTracks()[0].stop();
     }
 
     mediaStream = await navigator.mediaDevices.getUserMedia({ video: { deviceId } });
-
     const source = createMediaStreamSource(mediaStream);
-
     await session.setSource(source);
-
     source.setTransform(Transform2D.MirrorX);
-
     session.play();
-  };
+  }, []);
 
-  const attachCamerasToSelect = async (session) => {
+  const attachCamerasToSelect = useCallback(async (session) => {
+    if (!cameraSelectRef.current) return;
+    
     cameraSelectRef.current.innerHTML = '';
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter(({ kind }) => kind === 'videoinput');
@@ -59,22 +35,24 @@ const SnapCamera = () => {
       const option = document.createElement('option');
       option.value = camera.deviceId;
       option.text = camera.label;
-      cameraSelectRef.current.appendChild(option);
+      cameraSelectRef.current?.appendChild(option);
     });
 
     cameraSelectRef.current.addEventListener('change', (event) => {
       const deviceId = event.target.selectedOptions[0].value;
       setCameraKitSource(session, deviceId);
     });
-  };
+  }, [setCameraKitSource]);
 
-  const attachLensesToSelect = async (lenses, session) => {
+  const attachLensesToSelect = useCallback(async (lenses, session) => {
+    if (!lensSelectRef.current) return;
+    
     lensSelectRef.current.innerHTML = '';
     lenses.forEach((lens) => {
       const option = document.createElement('option');
       option.value = lens.id;
       option.text = lens.name;
-      lensSelectRef.current.appendChild(option);
+      lensSelectRef.current?.appendChild(option);
     });
 
     lensSelectRef.current.addEventListener('change', (event) => {
@@ -82,7 +60,26 @@ const SnapCamera = () => {
       const lens = lenses.find((lens) => lens.id === lensId);
       if (lens) session.applyLens(lens);
     });
-  };
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      const cameraKit = await bootstrapCameraKit({ apiToken });
+      const session = await cameraKit.createSession();
+
+      const canvas = canvasRef.current;
+      if (canvas) {
+        canvas.replaceWith(session.output.live);
+      }
+      const { lenses } = await cameraKit.lenses.repository.loadLensGroups([lensGroupId]);
+      session.applyLens(lenses[19]);
+      await setCameraKitSource(session);
+      await attachCamerasToSelect(session);
+      await attachLensesToSelect(lenses, session);
+    };
+
+    init();
+  }, [apiToken, lensGroupId, setCameraKitSource, attachCamerasToSelect, attachLensesToSelect]);
 
   return (
     <div className="container">
